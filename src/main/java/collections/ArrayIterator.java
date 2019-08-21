6d2ed6c2979b992c06daa3fc572a3e229a78cf91
Andrey <avedishchev@list.ru>
2019-08-21 19:01:10 +0300

    Добавление проекта

---
+++ src/main/java/collections/ArrayIterator.java
@@ -0,0 +1,38 @@
+package collections;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ArrayIterator implements Iterator {
+
+    private final int[][] values;
+    private int i;
+    private int j;
+
+    public ArrayIterator(final int[][] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (i < values.length && j < values[i].length);
+    }
+
+    @Override
+    public Object next() {
+
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+
+        int element = values[i][j];
+        j++;
+
+        while (i < values.length && j >= values[i].length) {
+            j = 0;
+            i++;
+        }
+
+        return element;
+    }
+}
